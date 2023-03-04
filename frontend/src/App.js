0c9bd87b96a11106451aa383ff8037e25e37a94d
i-Falinirina <44920333+ANDRIANANTENAINA@users.noreply.github.com>
2023-03-04 22:08:10 +0300

    backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import Navbar from "./component/NavBar/nav";
 import Home from "./component/Home/home";
+import LoginForm from "./component/Auth/LoginForm";
 function App() {
   return (
     <div className="App">
         <Navbar/>
         <Home/>
+
     </div>
+    //   <div className="bg-gray-100 min-h-screen">
+    //       <div className="flex justify-center items-center h-screen">
+    //           <LoginForm />
+    //       </div>
+    //   </div>
   );
 }
 
