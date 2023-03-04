4537e4cdcf3c7f808b37fa3b0a9d40fb6d846435
Herizo2004 <109790252+Herizo2004@users.noreply.github.com>
2023-03-05 04:03:19 +0600

    home

--- frontend/src/component/Home/home.js
+++ frontend/src/component/Home/home.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import './home.css'
 const Home = () =>{
     return(
         <div className="flex flex-row justify-between space-x-3 p-10">
@@ -9,7 +9,23 @@ const Home = () =>{
             </div>
 
             <div className="shadow-xl border-t rounded-xl w-2/3 h-screen pt-1 pl-1">
-                <p>ASIDE2</p>
+                <div className="card">
+                    <p className="text-gray-500 hover:text-gray-700 ">0 votes</p>
+                    <p className="text-indigo-800 hover:text-indigo-900 font-bold "> dolor sit amet, consectetur adipisicing elit. At commodi fugiat laboriosam, natus nemo</p>
+                    <p className="text-gray-500 hover:text-gray-700 ">0 answers</p>
+                    <div className="flex flex-row space-x-5 text-  ">
+                        <p className="bg-amber-400 pt-1 pb-1 pl-2 pr-2 rounded hover:bg-amber-600 ">tag1</p>
+                        <p className="bg-amber-400 pt-1 pb-1 pl-2 pr-2 rounded hover:bg-amber-600 ">tag1</p>
+                        <p className="bg-amber-400 pt-1 pb-1 pl-2 pr-2 rounded hover:bg-amber-600 ">tag1</p>
+                        <p className="bg-amber-400 pt-1 pb-1 pl-2 pr-2 rounded hover:bg-amber-600 ">tag1</p>
+                    </div>
+                    <p className="text-gray-500 hover:text-gray-700 ">0 views</p>
+                    <div className="flex flex-row space-x-5 ">
+                        <p>User</p>
+                        <p>Date</p>
+                    </div>
+                </div>
+                <hr className="mt-3"/>
             </div>
 
             <div className="shadow-xl border-t rounded-xl w-1/6 h-screen pt-1 pl-1">
