2b14b86a50ce732865c9fcbba82f987ae1f5f91a
Herizo2004 <109790252+Herizo2004@users.noreply.github.com>
2023-03-04 23:32:12 +0600

    fix bug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
-import Navbar from "./component/NavBar/nav";
 import Home from "./component/Home/home";
+import Navbar from "./component/NavBar/nav";
+import Chat from "./component/Chat/chat";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Profile from "./component/Profile/profile";
+
 function App() {
   return (
     <div className="App">
-        <Navbar/>
-        <Home/>
+        <Router>
+            <Navbar/>
+            <Routes>
+                <Route path='/home' element={<Home/>}/>
+                <Route path='/chat' element={<Chat/>}/>
+                <Route path='/profile' element={<Profile/>}/>
+            </Routes>
+        </Router>
     </div>
   );
 }
