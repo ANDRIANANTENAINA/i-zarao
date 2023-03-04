8be75b6feaf08a7d3f158c4321a730712b3a7c54
i-Falinirina <44920333+ANDRIANANTENAINA@users.noreply.github.com>
2023-03-04 22:06:14 +0300

    backend

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -21,6 +21,11 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'lastname',
+        'address',
+        'matricule',
+        'state',
+        'role',
     ];
 
     /**
