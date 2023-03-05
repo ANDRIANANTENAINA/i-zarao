8114c2c26fcf92de0f3b8808f337e15179f97c4b
Herizo2004 <109790252+Herizo2004@users.noreply.github.com>
2023-03-06 01:44:36 +0600

    chat scroll et fixe

--- frontend/src/component/Chat/chat.js
+++ frontend/src/component/Chat/chat.js
@@ -21,8 +21,8 @@ const Chat = () =>{
                     <textarea className="send_ms mx-5 rounded p-2 border-2 border-gray-200 " placeholder="vos question ?"></textarea>
                     <div className="btn_ms mx-5 mt-5 space-x-2">
                         <input type="file" id="file" className="input_file"/>
-                        <label for="file" className="green rounded px-4" > Ajouter un fichier</label>
-                        <button className="green px-4  rounded">Envoyer</button>
+                        <label for="file" className="bg-[#10A37F] p-2 shadow text-white font-bold rounded" > Parcourir</label>
+                        <button className="bg-[#1197f5] p-2 shadow text-white font-bold rounded">Envoyer</button>
                     </div>
                 </div>
             </div>
